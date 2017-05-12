d2db22d6e279e7f59ed5daac7679a423be3b343e
Tourkey <tourkey7@gmail.com>
2017-05-12 17:00:53 +0200

    adding types to MemIf_types.h, and using them in Ea.Cfg.h

--- EA/Config/Ea_Cfg.h
+++ EA/Config/Ea_Cfg.h
@@ -1,38 +1,34 @@
+#ifndef EA_CFG_H
+#define EA_CFG_H
+
 #include "NvM_Cbk.h"
 #include "MemIf_Types.h"
-#include "Eap.h"
+#include "Eep.h"
 
-
-#ifndef EA_GENERAL_H
-#define EA_GENERAL_H
 
 /*General configuration of the EEPROM abstraction module. This container 
 lists block independent configuration parameters.*/
-
-
-#define false 0
-#define true 1
 
 /*Switches the development error detection and notification on or off.
   true: detection and notification is enabled. 
   false: detection and notification is disabled */
 
-#define EaDevErrorDetect	false
+#define EaDevErrorDetect	(EcucBooleanParamDef)false
 
 
 /*The period between successive calls to the main function in seconds.*/
-#define EaMainFunctionPeriod 	10
+#define EaMainFunctionPeriod 	(EcucFloatParamDef)0.1
 
 
 
 /*Mapped to the job end notification routine provided by the upper layer 
 module (NvM_JobEndNotification).*/
-#define EaNvmJobEndNotification  
 
 
-/*Mapped to the job error notification routine provided by the upper layer 
-module (NvM_JobErrorNotification).*/
-#define EaNvmJobErrorNotification
+/* NVM Pooling mode disabled and interface provided to EA */
+#if NvMPollingMode == (EcucBooleanParamDef)false 
+#define EaNvmJobEndNotification NvM_JobEndNotification
+#endif
 
 
 /*Pre-processor switch to enable and disable the polling mode for this 
@@ -41,32 +37,25 @@ true: Polling mode enabled, callback functions (provided to EEP module)
 disabled.
 false: Polling mode disabled, callback functions (provided to EEP module) 
 enabled.*/
-#define EaPollingMode false
+#define EaPollingMode (EcucBooleanParamDef)false
 
 /*Compile switch to enable / disable the function Ea_SetMode.*/
-#define EaSetModeSupported true
+#define EaSetModeSupported (EcucBooleanParamDef)true
 
 /*Pre-processor switch to enable / disable the API to read out the modules
 version information.
 true: Version info API enabled.
 false: Version info API disabled
 */
-#define EaVersionInfoApi  false
+#define EaVersionInfoApi  (EcucBooleanParamDef)false
 
 
 
 
 /*The size in bytes to which logical blocks shall be aligned.*/
-#define EaVirtualPageSize  50
+#define EaVirtualPageSize  (EcucIntegerParamDef)50
 
-#endif /* EA_GENERAL_H */
-
-/////////////////////////////////////////
-
-
-
-
-
+///////////////////////////////////////////////
 
 /*Block identifier (handle).
 0x0000 and 0xFFFF shall not be used for block numbers (see EA006).
@@ -75,22 +64,22 @@ min = 2^NVM_DATASET_SELECTION_BITS
 max = 0xFFFF -2^NVM_DATASET_SELECTION_BITS
 Note: Depending on the number of bits set aside for dataset selection 
 several other block numbers shall also be left out to ease implementation.*/
-#define EaBlockConfiguration	50
+#define EaBlockNumber	(EcucIntegerParamDef)50
 
 
 /*Size of a logical block in bytes*/
-#define EaBlockSize  50
+#define EaBlockSize  (EcucIntegerParamDef)50
 
 
 
 /*Marker for high priority data.
 true: Block contains immediate data.
 false: Block does not contain immediate data.*/
-#define EaImmediateData false
+#define EaImmediateData  (EcucBooleanParamDef)false
 
 
 /*Number of write cycles required for this block*/
-#define EaNumberOfWriteCycles
+#define EaNumberOfWriteCycles (EcucIntegerParamDef)0
 
 /*Reference to the device this block is stored in*/
 #define EaDeviceIndex
@@ -99,21 +88,19 @@ false: Block does not contain immediate data.*/
 //////////////////////////////////////
 
 
-
 /*Management overhead per logical block in bytes.
 Note: If the management overhead depends on the block size or block 
 location a formula has to be provided that allows the configurator to 
 calculate the management overhead correctly.*/
 
 
-#define EaBlockOverhead
+#define EaBlockOverhead (EcucIntegerParamDef)0
 
 /*Management overhead per logical block in bytes.
 Note: If the management overhead depends on the block size or block 
 location a formula has to be provided that allows the configurator to 
 calculate the management overhead correctly.*/
-
-#define EaPageOverhead
+#define EaPageOverhead (EcucIntegerParamDef)0
 
 
 
@@ -121,6 +108,8 @@ calculate the management overhead correctly.*/
 Note: If the management overhead depends on the block size or block 
 location a formula has to be provided that allows the configurator to 
 calculate the management overhead correctly*/
-#define EaPageOverhead
+#define EaPageOverhead (EcucIntegerParamDef)0
 
 //////////////////////////////////////////////
+
+#endif /* EA_CFG_H */
---
+++ sharedTypes/MemIf_Types.h
@@ -0,0 +1,11 @@
+
+/* ECU types def */
+
+typedef enum{
+	false,
+	true
+}EcucBooleanParamDef;
+
+typedef float EcucFloatParamDef ;
+
+typedef int EcucIntegerParamDef ;
